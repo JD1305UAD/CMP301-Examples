84b41806bb0cc6d1922d93e48973eb90b67e27af
James Dick <1700277@uad.ac.uk>
2019-10-15 20:24:59 +0100

    attenuation

--- E4_Lighting2/E4_Lighting2/shaders/light_ps.hlsl
+++ E4_Lighting2/E4_Lighting2/shaders/light_ps.hlsl
@@ -21,19 +21,33 @@ struct InputType
 };
 
 // Calculate lighting intensity based on direction and normal. Combine with light colour.
-float4 calculateLighting(float3 lightDirection, float3 normal, float4 ldiffuse)
+float4 calculateLighting(float3 lightDirection, float3 normal, float4 ldiffuse, float attenuation)
 {
 	float intensity = saturate(dot(normal, lightDirection));
+	ldiffuse = ldiffuse * attenuation;
 	float4 colour = saturate(ldiffuse * intensity);
+	//colour = colour * attenuation;
 	return colour;
+}
+
+float calculateAttenuation(float distance)
+{
+	float constantFactor = 0.5f;
+	float linearFactor = 0.125f;
+	float quadraticFactor = 0.0f;
+	// 
+	float4 attenuation = 1 / (constantFactor + (linearFactor * distance) + (quadraticFactor * pow(distance, 2)));
+
+	return attenuation;
 }
 
 float4 main(InputType input) : SV_TARGET
 {
 	// Sample the texture. Calculate light intensity and colour, return light*texture for final pixel colour.
 	float4 textureColour = texture0.Sample(sampler0, input.tex);
+	float attenuation = calculateAttenuation(position - input.worldPosition);
 	float3 lightVector = normalize(position - input.worldPosition);
-	float4 lightColour = ambient + calculateLighting(lightVector, input.normal, diffuse);
+	float4 lightColour = ambient + calculateLighting(lightVector, input.normal, diffuse, attenuation);
 	
 	return lightColour * textureColour;
 }
